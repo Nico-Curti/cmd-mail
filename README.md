af0f597e9c1e3224923172e501c21ee145b280ff
Nico-Curti <nico.curti2@unibo.it>
2019-08-17 11:15:54 +0200

    update version

--- README.md
+++ README.md
@@ -1,22 +1,66 @@
-| **Authors**  | **Project** |
-|:------------:|:-----------:|
-|   N. Curti   |   cmd-mail  |
+| **Authors**                                   | **Project**     |
+|:---------------------------------------------:|:---------------:|
+| [**N. Curti**](https://github.com/Nico-Curti) |   **cmd-mail**  |
 
-# Send eMail
-### (for distracted/lazy people like me)
 
-The project includes different ways (in different languages) to send email by command line programming.
+# SMail :grin:
+### Send eMail
 
-In every script the function `smail` parse the file [mail.config](https://github.com/Nico-Curti/cmd-mail/blob/master/mail.config) in which informations about users and domains are stored.
+The project includes different ways (in different languages) to send email by using simple command line instructions.
+
+Before use this package, please edit the `mail.config` file with your credentials.
+
+1. [Prerequisites](#prerequisites)
+2. [Installation](#installation)
+3. [Authors](#authors)
+4. [License](#license)
+
+## Prerequisites
+
+In every script the function `smail` parse the configuration file [mail.config](https://github.com/Nico-Curti/cmd-mail/blob/master/mail.config) in which informations about users and domains are stored.
+
+If you are using the Python version install the full list of prerequisites using `pip install -r requirements.txt` in the project directory.
+
+All the other smile versions have no particular requirements.
+You can compile the C++ version with the simple command
+
+```
+g++ smail.cpp -o smail -O3
+```
 
 ## Languages
 
 The `smail` function is supported in:
 
 - [bash](https://github.com/Nico-Curti/cmd-mail/blob/master/smail.sh)
+
+```bash
+bash smail.sh to from "echo Hello World!"
+```
+
 - [powershell](https://github.com/Nico-Curti/cmd-mail/blob/master/smail.ps1)
+
+
+```pwsh
+./smail.ps1 -TO to -FROM from -cmd "echo Hello World!"
+```
+
 - [python](https://github.com/Nico-Curti/cmd-mail/blob/master/smail.py)
+
+```bash
+python smail.py -t to -f from -x "echo Hello World!"
+```
+
 - [c++](https://github.com/Nico-Curti/cmd-mail/blob/master/smail.cpp)
+
+
+```bash
+g++ smail.cpp -o smail -O3
+./smail to from "echo Hello World!"
+```
+
+The mail credentials in the configuration file are parsed and the command line was built using that informations. In addition you can add a command line to execute before the mail sending. In this way the output of the command was redirected into a log file which will be attached to mail.
+It can be useful when you are working on long pipeline and you want know when it finish.
 
 ## Authors
 
@@ -24,4 +68,4 @@ The `smail` function is supported in:
 
 ## License
 
-The Project is under GPL License.
+The `cmd-mail` package is released under GPL License. [![License](https://img.shields.io/badge/license-GPL-blue.svg?style=plastic)](https://github.com/Nico-Curti/rFBP/blob/master/LICENSE.md)
--- smail.py
+++ smail.py
@@ -1,69 +1,92 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
 
 import yaml
+import getpass
 import smtplib
+import argparse
+import subprocess
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
-import getpass
-import sys
-import argparse
-import subprocess
 
-if __name__ == "__main__":
-  description = "*-* Send Mail *-*"
+__package__ = 'email sender'
+__author__  = ['Nico Curti']
+__email__   = ['nico.curti2@unibo.it']
+
+
+def parse_args ():
+
+  description = '*-* Send Mail *-*'
   parser = argparse.ArgumentParser(description = description)
-  parser.add_argument("-t", required=True, dest="TO", action="store", help="mail.config key TO send e-mail")
-  parser.add_argument("-f", required=True, dest="FROM", action="store", help="mail.config key FROM send e-mail")
-  parser.add_argument("-x", required=True, dest="cmd", action="store", help="command line to run")
 
-  if len(sys.argv) <= 1:
-    parser.print_help()
-    sys.exit(1)
-  else:
-    args = parser.parse_args()
+  parser.add_argument('-t', required=True, type=str, dest='TO', action='store', help='mail.config key TO send e-mail')
+  parser.add_argument('-f', required=True, type=str, dest='FROM', action='store', help='mail.config key FROM send e-mail')
+  parser.add_argument('-x', required=True, type=str, dest='cmd', action='store', help='command line to run')
 
-  config = yaml.load(open("mail.config", "r"))
+  args = parser.parse_args()
+
+  return args
+
+
+
+
+if __name__ == '__main__':
+
+
+  args = parse_args()
+
+  with open('mail.config', 'r') as fp:
+    config = yaml.load(fp)
+
   FROM = config[args.FROM]
   TO = config[args.TO]
   cmd = args.cmd
 
-  pswd = getpass.getpass("Password %s: " %(FROM["address"]))
+  pswd = getpass.getpass('Password {}: '.format(FROM['address']))
 
   msg = MIMEMultipart()
-  msg["From"] = FROM["username"]
-  msg["To"] = TO["username"]
-  msg["Subject"] = "Results run %s"%(cmd.split(" ")[0])
+  msg['From'] = FROM['username']
+  msg['To'] = TO['username']
+  msg['Subject'] = 'Results run {}'.format(cmd.split(' ')[0])
 
   proc = subprocess.Popen([cmd], stdout=subprocess.PIPE, shell=True)
   (out, err) = proc.communicate()
-  with open("log.txt", "wb") as f:
-    f.write(out)
-  if err == None:
-    msg.attach(MIMEText("Success! Script exit with 0. For the output stream see the logfile.", "plain"))
-  else:
-    msg.attach(MIMEText("Somethig goes wrong. Please see the logfile for more informations."))
 
-  with open("log.txt", "rb") as f:
-    part = MIMEApplication(f.read(), Name="log.txt")
+  with open('log.txt', 'wb') as f:
+    f.write(out)
+
+  if err == None:
+    msg.attach(MIMEText('Success! Script exit with 0. For the output stream see the logfile.', 'plain'))
+
+  else:
+    msg.attach(MIMEText('Somethig goes wrong. Please see the logfile for more informations.'))
+
+  with open('log.txt', 'rb') as f:
+    part = MIMEApplication(f.read(), Name='log.txt')
 
   # After the file is closed
   part['Content-Disposition'] = 'attachment; filename="log.txt"'
   msg.attach(part)
 
-  print("Send e-mail... ", end="")
+  print('Send e-mail... ', end='', flush=True)
+
   try:
-    server = smtplib.SMTP(FROM["smtp"], 25)
-    server.connect(FROM["smtp"], int(FROM["port"]))
+
+    server = smtplib.SMTP(FROM['smtp'], 25)
+    server.connect(FROM['smtp'], int(FROM['port']))
     server.ehlo()
     server.starttls()
     server.ehlo()
-    server.login(FROM["address"], pswd)
+    server.login(FROM['address'], pswd)
 
     text = msg.as_string()
-    server.sendmail(FROM["address"], TO["address"], text)
+    server.sendmail(FROM['address'], TO['address'], text)
     server.quit()
-    print("[DONE]")
+    print('[DONE]')
+
   except:
-    print("[FAILED]")
+    print('[FAILED]')
 
